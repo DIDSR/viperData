49066d9221971d606c5518e5a24025b754de4e23
Brandon Gallas <brandon.gallas@fda.hhs.gov>
2024-08-07 08:09:54 -0400

    More columns and better description

--- inst/extra/demo/prepScreeningLowP.R
+++ inst/extra/demo/prepScreeningLowP.R
@@ -1,4 +1,24 @@
 # Extract "screeningLowP" sub-study data
+# There are 6911 observations from 20 readers and 419 cases
+# The study design is a split-plot study with four groups
+# that have overlapping cases to create a low prevalence study population.
+# The truth (reference standard) for these cases is defined
+# separately in the casesViper data object
+
+# The data frame follows the standard iMRMC format with these 4 columns:
+#   readerID
+#   caseID
+#   modalityID
+#   score
+#   recall - 1 recall (score <= 100), 2 do not recall (score > 100)
+#   abnormalityType - multi-option labels 
+#       Architectural Distortion
+#       Asymmetry
+#       Mass
+#       Microcalcifications
+#       No Lesion (Quality Issue)
+#   caseGroup - label to identify split-plot groups within a sub-study
+#   subStudyLabel - label to identify split-plot groups within a sub-study
 
 
 
@@ -16,7 +36,15 @@ mrmcDF <- viperData::viperObs
 mrmcDF <- mrmcDF[mrmcDF$subStudyLabel == "screeningLowP", ]
 
 # Simplify the data frame to the key MRMC analysis variables and re-factor
-mrmcDF <- mrmcDF[, c("readerID", "caseID", "modalityID", "score")]
+mrmcDF <- mrmcDF[, c(
+  "readerID",
+  "caseID",
+  "modalityID",
+  "score",
+  "recall",
+  "abnormalityType",
+  "caseGroup",
+  "subStudyLabel")]
 mrmcDF$readerID <- factor(mrmcDF$readerID)
 mrmcDF$caseID <- factor(mrmcDF$caseID)
 mrmcDF$modalityID <- factor(mrmcDF$modalityID)
